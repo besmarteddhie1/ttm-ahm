2b8c488fe274b3c016d6f75b61775743c2a386de
Edy A <edy.apriansyah@atiila.co.id>
2016-03-23 03:56:51 +0700

    .

--- src/main/java/com/ahm/jx/ttm/services/AccountService.java
+++ src/main/java/com/ahm/jx/ttm/services/AccountService.java
@@ -20,55 +20,67 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import com.ahm.jx.ttm.dao.AccountDao;
-import com.ahm.jx.ttm.entities.Account;
+import com.ahm.jx.ttm.dao.UamRoleDao;
+import com.ahm.jx.ttm.dao.UamUserDao;
+import com.ahm.jx.ttm.entities.UamRole;
+import com.ahm.jx.ttm.entities.UamUser;
+import com.ahm.jx.ttm.entities.UamUserRole;
 
 @Service
 @Scope(proxyMode = ScopedProxyMode.TARGET_CLASS)
 public class AccountService implements UserDetailsService {
 	
 	@Autowired
-	private AccountDao accountRepository;
+	private UamUserDao userRepository;
+	
+	@Autowired
+	private UamRoleDao roleRepository;	
 
-	//@Autowired
 	private Md5PasswordEncoder passwordEncoder = new Md5PasswordEncoder();
 
 	@PostConstruct	
 	public void initialize() {
-		save(new Account("user", "demo", "ROLE_USER"));
-		save(new Account("admin", "admin", "ROLE_ADMIN"));
+		save(new UamUser("user", "demo"), "ROLE_USER");
+		save(new UamUser("admin", "admin"), "ROLE_ADMIN");
 	}
 
 	@Transactional
-	public Account save(Account account) {
+	public UamUser save(UamUser account, String role) {
+		UamRole r = roleRepository.findOneByIdRole(role);
+		if (r != null) {
+			UamUserRole ur = new UamUserRole();
+			ur.setRole(r);
+			ur.setUser(account);
+			account.getUserRoles().add(ur);
+		}
 		account.setPassword(passwordEncoder.encodePassword(account.getPassword(), null));
-		accountRepository.save(account);
+		userRepository.save(account);
 		return account;
 	}
 
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		Account account = accountRepository.findOneByEmail(username);
-		if(account == null) {
-			throw new UsernameNotFoundException("user not found");
+		UamUser account = userRepository.findOneByUserName(username);
+		if(account == null) {			
+			throw new UsernameNotFoundException(username + " in data not found");
 		}
 		return createUser(account);
 	}
 	
-	public void signin(Account account) {
+	public void signin(UamUser account) {
 		SecurityContextHolder.getContext().setAuthentication(authenticate(account));
 	}
 	
-	private Authentication authenticate(Account account) {
+	private Authentication authenticate(UamUser account) {
 		return new UsernamePasswordAuthenticationToken(createUser(account), null, Collections.singleton(createAuthority(account)));		
 	}
 	
-	private User createUser(Account account) {
-		return new User(account.getEmail(), account.getPassword(), Collections.singleton(createAuthority(account)));
+	private User createUser(UamUser account) {
+		return new User(account.getUserName(), account.getPassword(), Collections.singleton(createAuthority(account)));
 	}
 
-	private GrantedAuthority createAuthority(Account account) {
-		return new SimpleGrantedAuthority(account.getRole());
+	private GrantedAuthority createAuthority(UamUser account) {
+		return new SimpleGrantedAuthority(account.getRoles().toString());
 	}
 
 }
