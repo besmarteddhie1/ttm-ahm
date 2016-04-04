a13d9fcb7e2497266b83c6bf84194c20571f3d65
Edy A <edy.apriansyah@atiila.co.id>
2016-04-04 17:41:48 +0700

    .

--- src/main/java/com/ahm/jx/ttm/entities/UamRoleMenu.java
+++ src/main/java/com/ahm/jx/ttm/entities/UamRoleMenu.java
@@ -1,12 +1,12 @@
 package com.ahm.jx.ttm.entities;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-/**
- *
+/*
  * @author sigit refactored by apri
  */
 
@@ -16,12 +16,18 @@ public class UamRoleMenu extends AhmBaseEntity {
 
 	private static final long serialVersionUID = -4031508059474896902L;
 	
+	@Column(name = "vid_ahmjxuam_mstroles")
+	private String idRole;
+	
+	@Column(name = "vid_ahmjxuam_mstmenus")
+	private String idMenu;	
+	
 	@ManyToOne
-	@JoinColumn(name = "vid_ahmjxuam_mstroles", referencedColumnName="vid")
+	@JoinColumn(name = "vid_ahmjxuam_mstroles", referencedColumnName="vid", insertable=false, updatable=false)
 	private UamRole role;
 	
 	@ManyToOne
-	@JoinColumn(name = "vid_ahmjxuam_mstmenus", referencedColumnName="vid")
+	@JoinColumn(name = "vid_ahmjxuam_mstmenus", referencedColumnName="vid", insertable=false, updatable=false)
 	private UamMenu menu;
 
 	public UamRole getRole() {
