8489612bcc6ee74e9095fe4743f5e7df2fa60660
Edy A <edy.apriansyah@atiila.co.id>
2016-04-11 15:26:35 +0700

    Add Sales Force Controller

--- src/main/java/com/ahm/jx/ttm/dao/AhmjxmstPartyCategoryDao.java
+++ src/main/java/com/ahm/jx/ttm/dao/AhmjxmstPartyCategoryDao.java
@@ -1,8 +1,11 @@
 package com.ahm.jx.ttm.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
 import com.ahm.jx.ttm.model.AhmjxmstPartyCategory;
 
+@Repository
 public interface AhmjxmstPartyCategoryDao extends JpaRepository<AhmjxmstPartyCategory, Integer> {
 
 }
--- src/main/java/com/ahm/jx/ttm/dao/AhmjxmstProductCategoryDao.java
+++ src/main/java/com/ahm/jx/ttm/dao/AhmjxmstProductCategoryDao.java
@@ -1,9 +1,11 @@
 package com.ahm.jx.ttm.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.ahm.jx.ttm.model.AhmjxmstProductCategory;
 
+@Repository
 public interface AhmjxmstProductCategoryDao extends JpaRepository<AhmjxmstProductCategory, Integer> {
 
 }
