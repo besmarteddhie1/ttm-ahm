5d4db631b33a98dd642188ba287d77e0f83c359e
Edy A <edy.apriansyah@atiila.co.id>
2016-04-11 15:11:40 +0700

    Add Party and Product Category DAO

---
+++ src/main/java/com/ahm/jx/ttm/dao/AhmjxmstPartyCategoryDao.java
@@ -0,0 +1,8 @@
+package com.ahm.jx.ttm.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import com.ahm.jx.ttm.model.AhmjxmstPartyCategory;
+
+public interface AhmjxmstPartyCategoryDao extends JpaRepository<AhmjxmstPartyCategory, Integer> {
+
+}
---
+++ src/main/java/com/ahm/jx/ttm/dao/AhmjxmstProductCategoryDao.java
@@ -0,0 +1,9 @@
+package com.ahm.jx.ttm.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ahm.jx.ttm.model.AhmjxmstProductCategory;
+
+public interface AhmjxmstProductCategoryDao extends JpaRepository<AhmjxmstProductCategory, Integer> {
+
+}
