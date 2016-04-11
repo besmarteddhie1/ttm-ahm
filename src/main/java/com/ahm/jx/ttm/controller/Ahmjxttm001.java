b70370ad41497b9af8b95ee747f537a98f4a5190
Edy A <edy.apriansyah@atiila.co.id>
2016-04-11 15:33:55 +0700

    .

--- src/main/java/com/ahm/jx/ttm/controller/Ahmjxttm001.java
+++ src/main/java/com/ahm/jx/ttm/controller/Ahmjxttm001.java
@@ -3,7 +3,9 @@ package com.ahm.jx.ttm.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ahm.jx.ttm.dao.AhmjxmstPartyCategoryDao;
@@ -21,9 +23,14 @@ public class Ahmjxttm001 {
 	@Autowired
 	AhmjxmstProductCategoryDao productCategoryRepo;
 	
-	@RequestMapping(value="sf")
+	@RequestMapping(value="sf", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE)
 	public List<AhmjxmstPartyCategory> getSalesForce() {
 		return partyCategoryRepo.findByParentCategoryType(AhmjxmstPartyCategoryType.TYPE_SALES_FORCE);
 	}
+	
+	@RequestMapping(value="sf", method = RequestMethod.POST, produces = MediaType.APPLICATION_JSON_VALUE)
+	public AhmjxmstPartyCategory setSalesForce(AhmjxmstPartyCategory entity) {
+		return partyCategoryRepo.save(entity);
+	}	
 	
 }
