2ac84275b7c3aa37b2bb114083b9a08116a890b1
Edy A <edy.apriansyah@atiila.co.id>
2016-04-12 17:34:57 +0700

    .

--- src/main/java/com/ahm/jx/ttm/repository/AhmjxmstPersonRepository.java
+++ src/main/java/com/ahm/jx/ttm/repository/AhmjxmstPersonRepository.java
@@ -2,7 +2,6 @@ package com.ahm.jx.ttm.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.ahm.jx.ttm.model.AhmjxmstPerson;
 
 @Repository
