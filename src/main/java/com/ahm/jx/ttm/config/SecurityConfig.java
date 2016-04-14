9aa30650624e3e4eb02d2d60de60f214f60afa83
apri <apri@besmart-universal.com>
2016-04-14 16:03:20 +0700

    .

--- src/main/java/com/ahm/jx/ttm/config/SecurityConfig.java
+++ src/main/java/com/ahm/jx/ttm/config/SecurityConfig.java
@@ -5,7 +5,6 @@ import javax.sql.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.encoding.Md5PasswordEncoder;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
