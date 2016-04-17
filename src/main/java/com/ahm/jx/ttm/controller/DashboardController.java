12a1f494b6f4dbbc2e8f1575c76671776cd8c50f
apri <apri@besmart-universal.com>
2016-04-18 04:18:45 +0700

    Added login controller

--- src/main/java/com/ahm/jx/ttm/controller/DashboardController.java
+++ src/main/java/com/ahm/jx/ttm/controller/DashboardController.java
@@ -1,7 +1,5 @@
 package com.ahm.jx.ttm.controller;
 
-import java.security.Principal;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,8 +8,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class DashboardController {
 	
 	@RequestMapping(value = "/dashboard", method = RequestMethod.GET)
-	public String dashboard(Principal principal) {
-		return principal != null ? "dashboard" : "login";
+	public String dashboard() {
+		return "dashboard";
 	}
 
 }
